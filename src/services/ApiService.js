7d255643991e5e4a4ec79429fc9b74362fe14164
Fitri HY <155282015+fitri-hy@users.noreply.github.com>
2024-06-11 14:01:29 +0700

    update

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,10 +1,72 @@
 class ApiService {
+
+  // GET Data from API
   static async getData(url) {
     try {
       const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       return await response.json();
     } catch (error) {
       console.error('Error fetching data:', error);
+      return [];
+    }
+  }
+  
+  // POST Data from API
+  static async postData(url, data) {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error posting data:', error);
+      return null;
+    }
+  }
+
+  // PUT Data from API
+  static async updateData(url, data) {
+    try {
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error updating data:', error);
+      return null;
+    }
+  }
+
+  // DELETE Data from API
+  static async deleteData(url) {
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Error deleting data:', error);
+      return null;
     }
   }
 }
